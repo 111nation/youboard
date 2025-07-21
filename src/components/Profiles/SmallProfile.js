d627c958971c8f8d9c30d982ebf263b095d7173d
111nation <tafara.k@outlook.com>
2025-07-21 12:25:39 +0000

    Added profile components

--- src/components/Profiles/SmallProfile.js
+++ src/components/Profiles/SmallProfile.js
@@ -1,5 +1,14 @@
+import "./Profile.css"
+
 function SmallProfile() {
 	return (
+		<div className="small-profile-wrap">
+			<img className="profile"/>
+			<div className="info-wrap">
+				<p className="handle">sadboy</p>
+				<p className="follower-count">92 Followers | 5 Following</p>
+			</div>
+		</div>
 	);
 }
 
