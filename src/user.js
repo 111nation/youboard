878625ab424c289be8585d1b8c0a3ad95f50a094
111nation <tafara.k@outlook.com>
2025-09-09 14:30:21 +0000

    Updated user class in user.js

--- src/user.js
+++ src/user.js
@@ -1,13 +1,29 @@
-import { AuthErrorCodes, createUserWithEmailAndPassword } from "firebase/auth";
+import { AuthErrorCodes, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import {auth, db} from "./firebase";
-import {doc, setDoc} from "firebase/firestore";
+import {doc, getDoc, setDoc} from "firebase/firestore";
 
-class User {
-	constructor(uid, username, email, docRef) {
+/* TODO: 
+ * Error handling for when NULL document returned
+ */
+
+async function getDocFromUsername(username) {
+	const q = query(
+		collection(db, "users"),
+		where("username", "==", username),
+		limit(1),
+	);
+	
+	const docs = await getDocs(q); // Doc can be empty
+
+	return docs[0];
+}
+
+export class User {
+	constructor(uid, username, email, userDoc) {
 		this.uid = uid;
 		this.username = username;
 		this.email = email;
-		this.docRef = docRef;
+		this.userDoc = userDoc;
 	}
 
 	static async createNewUser(username, email, password) {
@@ -19,11 +35,50 @@ class User {
 			username: username,
 			email: email,
 		});
+		const userDoc = await getDoc(docRef);
 
-		return new User(uid, username, email, docRef);
+		return new User(user.uid, username, email, userDoc);
+	}
+
+	static async signInWithEmail(email, password) {
+		let userCred = await signInWithEmailAndPassword(auth, email, password);
+		const user = userCred.user;
+		const docRef = doc(db, "users", user.uid);
+		const userDoc = await getDoc(docRef); // Possible error that doc is empty
+		const username = docRef.data().username;
+
+		return new User(user.uid, username, email, userDoc);
 	}
 
 	static async signInWithUsername(username, password) {
-		
+		const userDoc = await getDocFromUsername(username);
+		const email = userDoc.data().email;
+	
+		let userCred = await signInWithEmailAndPassword(auth, email, password);
+		const user = userCred.user;
+
+		return new User(user.uid, username, email, userDoc);
+	}
+
+	/* Initialize non logged in user
+	 * Used to grab other user's data
+	 */
+
+	static async getFromUid(uid) {
+		const docRef = doc(db, "users", uid);
+		const userDoc = await getDoc(docRef); // Possible error that doc is empty
+		const username = userDoc.data().username;
+		const email = userDoc.data().email;
+
+		return new User(uid, username, email, userDoc);
+	}
+
+	static async getFromUsername(username) {
+		const userDoc = await getDocFromUsername(username); // Possible error that doc is empty
+		const email = userDoc.data().email;
+		const uid = userDoc.id;
+
+		return new User(uid, username, email, userDoc);
 	}
 };
+
